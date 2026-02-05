c19653a59d75e62af3e3c89978ffe56494407451
lakshman-tech-dev <codelaksh12@gmail.com>
2026-02-05 23:13:11 +0530

    Comments in C++

---
+++ comments.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //print a name
+    cout<<"My name is lakshman";
+    /*lakshman is from
+    Alpha matric.hr.
+    secondary school*/
+}

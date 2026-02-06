4070801abe1878433695a6dbaf0a8e4349f61e8d
lakshman-tech-dev <codelaksh12@gmail.com>
2026-02-06 21:55:44 +0530

    Variables initailisation,declaration, assignment in C++

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //variable declaration
+    int age;//initially age will have garbage value random 0s and 1s(binaries)
+    cin>>age;
+    cout<<"The boy age is:"<<age<<endl;
+    int a=10;//variable initialisation + declaration
+    int b,c,d;
+    cin>>b>>c;
+    cout<<"The sum is:"<<a+b+c<<endl;
+    d=100;//Assignment
+    cout<<d;
+
+}

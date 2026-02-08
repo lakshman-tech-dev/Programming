9b172709cfeb777a140cf26ab588f3b9dc4e0a74
lakshman-tech-dev <codelaksh12@gmail.com>
2026-02-09 00:18:07 +0530

    Demonstrated relational operators in C++

---
+++ Relational_operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Relational operators >,<,<=,>=,==,!=
+    int a=10;
+    int b=5;
+    //used for comparison between two operands
+    cout<<(a>b)<<endl;//1
+    cout<<(a<b)<<endl;//0
+    cout<<(a>=b)<<endl;//greater than or equal to 1
+    cout<<(a<=b)<<endl;//0
+    cout<<(a==b)<<endl;//0
+    cout<<(a!=b)<<endl;//1
+    return 0;
+}

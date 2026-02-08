2c23d19cb38837454d99201b95b4596cfaabd4bf
lakshman-tech-dev <codelaksh12@gmail.com>
2026-02-09 01:16:26 +0530

    Demonstrated if statement in C++

---
+++ ifblock.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //if block in c++ execute block of code only if condition is true
+    int phone = 15,money; //15K
+    cin>>money;
+    if(money>phone)
+    {
+        cout<<"Lets buy this !!"<<endl;
+    }
+    cout<<"Thanks for shopping !!";
+    return 0;
+}

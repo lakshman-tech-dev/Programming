d4321fa112556d14de58f3eddfa59d19007ba2e2
lakshman-tech-dev <codelaksh12@gmail.com>
2026-02-08 23:28:24 +0530

    Demonstrated compound assignment examples in C++

---
+++ Compound_assignment.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //compound assignment operators +=,-=,*=,/=,%=
+    int a=10;
+    cout<<"Compound assignment examples:"<<endl;
+    a+=7; //a=a+7
+    cout<<a<<endl;
+    a-=3;//a=a-3
+    cout<<a<<endl;
+    a*=3;//a=a*3
+    cout<<a<<endl;
+    a/=3;//a=a/3
+    cout<<a<<endl;
+    a%=2;//show remainder:a=a%2
+    cout<<a<<endl;
+    return 0;
+
+}

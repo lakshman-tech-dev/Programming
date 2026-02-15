9ed49006dfdfde17b664fcb4c8140aba19ec614b
lakshman-tech-dev <codelaksh12@gmail.com>
2026-02-15 13:12:17 +0530

    Demonstrated function return values

---
+++ Returnvalues.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+    return a+b;
+}
+int main()
+{
+    int a=10;
+    int b=20;
+    int result=add(a,b);
+    cout<<"Result:"<<result<<endl;
+}
